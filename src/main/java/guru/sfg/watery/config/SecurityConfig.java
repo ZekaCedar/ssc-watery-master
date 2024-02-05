fd7b105a71a09e15460b0f2d1393cdd57f88e06c
ZekaCedar <zulaikha.affandi@gmail.com>
2024-02-05 13:43:33 +0800

    Add custom authentication filter restHeaderAuthFilter

--- src/main/java/guru/sfg/watery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/watery/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package guru.sfg.watery.config;
 
+import guru.sfg.watery.security.RestHeaderAuthFilter;
 import guru.sfg.watery.security.SfgPasswordEncoderFactories;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -18,45 +19,30 @@ import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    /**
-     * Creates a {@link DelegatingPasswordEncoder} with default mappings. Additional
-     * mappings may be added and the encoding will be updated to conform with best
-     * practices. However, due to the nature of {@link DelegatingPasswordEncoder} the
-     * updates should not impact users. The mappings current are:
-     *
-     * <ul>
-     * <li>bcrypt - {@link BCryptPasswordEncoder} (Also used for encoding)</li>
-     * <li>ldap - {@link org.springframework.security.crypto.password.LdapShaPasswordEncoder}</li>
-     * <li>MD4 - {@link org.springframework.security.crypto.password.Md4PasswordEncoder}</li>
-     * <li>MD5 - {@code new MessageDigestPasswordEncoder("MD5")}</li>
-     * <li>noop - {@link org.springframework.security.crypto.password.NoOpPasswordEncoder}</li>
-     * <li>pbkdf2 - {@link Pbkdf2PasswordEncoder}</li>
-     * <li>scrypt - {@link SCryptPasswordEncoder}</li>
-     * <li>SHA-1 - {@code new MessageDigestPasswordEncoder("SHA-1")}</li>
-     * <li>SHA-256 - {@code new MessageDigestPasswordEncoder("SHA-256")}</li>
-     * <li>sha256 - {@link org.springframework.security.crypto.password.StandardPasswordEncoder}</li>
-     * <li>argon2 - {@link Argon2PasswordEncoder}</li>
-     * </ul>
-     *
-     * @return the {@link PasswordEncoder} to use
-     */
-    @Bean
-    PasswordEncoder passwordEncoder(){
-//        return NoOpPasswordEncoder.getInstance();
-//        return new LdapShaPasswordEncoder();
-//        return  new StandardPasswordEncoder();
-//        return  new BCryptPasswordEncoder();
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-          return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+    public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager){
+        RestHeaderAuthFilter filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
+        filter.setAuthenticationManager(authenticationManager);
+        return filter;
     }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        // Set to Use Customize Authentication filter
+        // Add this filter in the filter chain just before the UsernamePasswordAuthenticationFilter
+        http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
+                UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable();
+
 
         http.authorizeRequests(authorize -> {
                     authorize
@@ -146,6 +132,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .password("{noop}tiger")
 //                .password("tiger")
                 .roles("CUSTOMER");
+    }
+
+    /**
+     * Creates a {@link DelegatingPasswordEncoder} with default mappings. Additional
+     * mappings may be added and the encoding will be updated to conform with best
+     * practices. However, due to the nature of {@link DelegatingPasswordEncoder} the
+     * updates should not impact users. The mappings current are:
+     *
+     * <ul>
+     * <li>bcrypt - {@link BCryptPasswordEncoder} (Also used for encoding)</li>
+     * <li>ldap - {@link org.springframework.security.crypto.password.LdapShaPasswordEncoder}</li>
+     * <li>MD4 - {@link org.springframework.security.crypto.password.Md4PasswordEncoder}</li>
+     * <li>MD5 - {@code new MessageDigestPasswordEncoder("MD5")}</li>
+     * <li>noop - {@link org.springframework.security.crypto.password.NoOpPasswordEncoder}</li>
+     * <li>pbkdf2 - {@link Pbkdf2PasswordEncoder}</li>
+     * <li>scrypt - {@link SCryptPasswordEncoder}</li>
+     * <li>SHA-1 - {@code new MessageDigestPasswordEncoder("SHA-1")}</li>
+     * <li>SHA-256 - {@code new MessageDigestPasswordEncoder("SHA-256")}</li>
+     * <li>sha256 - {@link org.springframework.security.crypto.password.StandardPasswordEncoder}</li>
+     * <li>argon2 - {@link Argon2PasswordEncoder}</li>
+     * </ul>
+     *
+     * @return the {@link PasswordEncoder} to use
+     */
+    @Bean
+    PasswordEncoder passwordEncoder(){
+//        return NoOpPasswordEncoder.getInstance();
+//        return new LdapShaPasswordEncoder();
+//        return  new StandardPasswordEncoder();
+//        return  new BCryptPasswordEncoder();
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     /**
--- src/test/java/guru/sfg/watery/web/controllers/api/WaterRestControllerIT.java
+++ src/test/java/guru/sfg/watery/web/controllers/api/WaterRestControllerIT.java
@@ -4,11 +4,19 @@ import guru.sfg.watery.web.controllers.BaseIT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 public class WaterRestControllerIT extends BaseIT {
+
+    @Test
+    void deleteWater() throws Exception {
+        mockMvc.perform(delete("/api/v1/water/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                .header("Api-Key","spring").header("Api-Secret","guru"))
+                .andExpect(status().isOk());
+    }
 
     @Test
     void findWaters() throws Exception{
